8ec4df49ca390c6013d3b87b06e807904e792902
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-02-11 13:55:32 +0300

    finish up on header component

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
     return (
         <div className="gpt3__header section__padding" id="home">
             <div className="gpt3__header-content">
-                <h1 className="gradient__text">let's Build Something amazing with GPT3 OpenAI</h1>
-                <p>yet bed any for travelling assistance indulgence unpleasing. Not thoughts all excercise blessing. Indulgence way everything joy alteration boisterous the attachement. Party we years to orderallow asked of.</p>
+                <h1 className="gradient__text">Let's Build Something amazing with GPT3 OpenAI</h1>
+                <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all excercise blessing. Indulgence way everything joy alteration boisterous the attachement. Party we years to orderallow asked of.</p>
                 <div className="gpt3__header-content__input">
                     <input type="email" placeholder='Your Email Address' />
                     <button type='button'>Get Started</button>
